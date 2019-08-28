a50d8f570fa3f76d008627cbf22fd6ed4c5e5abf
liujingpeng <liujingpeng@syberos.com>
2019-08-28 13:21:23 +0800

    增加从环境变量中设置配置文件路径的方式，方便调试时使用

--- core/config.go
+++ core/config.go
@@ -3,12 +3,16 @@ package core
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"time"
 )
+
+//环境变量配置的spm配置文件路径
+const SpmConfigEnvVar = "SPM_CONFIG"
 
 type Database struct {
 	Name string 	`json:"name" yaml:"name"`
@@ -75,7 +79,7 @@ func (c *Config) loadFromJson(path string) error{
 	return nil
 }
 
-func (c *Config) loadFormYml(path string) error{
+func (c *Config) loadFromYml(path string) error{
 	data, err := ioutil.ReadFile(path)
 	if err!=nil {
 		return err
@@ -98,19 +102,55 @@ func (c *Config) currentPath() (string, error) {
 	return filepath.Dir(dir), nil
 }
 
+//从环境变量中获取配置文件路径
+func (c *Config) fileFromEnvVar() string{
+	spmConfigPath := os.Getenv(SpmConfigEnvVar)
+	if spmConfigPath!="" {
+		if IsExists(spmConfigPath) {
+			return spmConfigPath
+		}
+	}
+	return ""
+}
 
+func (c *Config) LoadFromEnvVar() (bool, error){
+	configFile := c.fileFromEnvVar()
+	if configFile=="" {
+		return false, nil
+	}
+	fmt.Println("load config from environment variable:", SpmConfigEnvVar, configFile)
+	extName := filepath.Ext(configFile)
+	if extName==".yaml" || extName==".yml" {
+		return true, c.loadFromYml(configFile)
+	}else if extName==".json" {
+		return true, c.loadFromJson(configFile)
+	}
+	return false, nil
+}
+
+//Load 加载配置文件，先尝试从环境变量中的配置文件位置中读取，再尝试从程序所在位置下的config目录中读取
 func (c *Config) Load() error{
+	//首先，尝试从环境变量中读取配置文件
+	isLoad, err := c.LoadFromEnvVar()
+	if err!=nil {
+		return err
+	}
+	if isLoad {
+		return nil
+	}
+	//从当前目录下读取
 	currentPath, err := c.currentPath()
 	if err!=nil {
 		return err
 	}
+	fmt.Println("load config from:", currentPath)
 	configFile := filepath.Join(currentPath, "config", "spm.yml")
 	if IsExists(configFile) {
-		return c.loadFormYml(configFile)
+		return c.loadFromYml(configFile)
 	}
 	configFile = filepath.Join(currentPath, "config", "spm.yaml")
 	if IsExists(configFile) {
-		return c.loadFormYml(configFile)
+		return c.loadFromYml(configFile)
 	}
 	configFile = filepath.Join(currentPath, "config", "spm.json")
 	if IsExists(configFile) {
